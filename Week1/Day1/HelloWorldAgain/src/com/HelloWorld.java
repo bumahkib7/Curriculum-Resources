867b46c43c841a286d118648fc9bb24dee4c0375
BenArayathel <ben.arayathel@gmail.com>
2022-02-07 10:47:19 -0500

    Day 1 Hello World and JDK

---
+++ Week1/Day1/HelloWorldAgain/src/com/HelloWorld.java
@@ -0,0 +1,71 @@
+package com;
+
+//This is a comment 
+
+/*
+ * This is a multi-line comment 
+ * 
+ * Allows us to add comments to our code to better explain our code!
+ * 
+ * The Java Virtual Machine ignores comments
+ * 
+ * Hello!
+ * 
+ * Java is a Strongly typed, compiled, OOP, general purpose programming language.
+	Java is used in enterprise settings, as a backend for 
+		full stack server applications.
+	Java has useful features such as Garbage Collection, JVM (WORA), Open source, 
+		supported by Oracle and its a popular language, so it has a lot, LOT 
+		of frameworks and libraries
+ */
+
+
+public class HelloWorld {
+	
+	//The special "main" method
+	public static void main(String[] args) {
+		//The JVM will see the main method as the "start line"
+		
+		System.out.println("Hello World!");
+		
+		
+		
+		
+		
+		/*
+		 * What is the JDK, JRE, JVM? 
+		 * 
+		 * JDK: Java Development Kit
+		 * 		Is designed to compile and run Java Code/Applications. 
+		 * 		It is designed for developers to be able to create 
+		 * 			Java Applications. 
+		 * 		
+		 *  	JDK contains the JRE (JRE is necessary for running code)
+		 * 		JDK comes with a lot of useful tools, such as the compiler!
+		 * 
+		 * JRE: Java Runtime Environment
+		 * 		JRE is what we need to run our Java applications. 
+		 * 		(WE can't build Java app with just the JRE, but we can run it)
+		 * 		ANYONE that doesn't need to code. 
+		 * 		With the JRE, I can run any compiled (bytecode) applications. 
+		 * 
+		 * 		JRE contains core system libraries 
+		 * 		JRE contains the JVM!
+		 * 
+		 * JVM: Java Virtual Machine
+		 * 	
+		 * 		JVM is what allows us to have WORA (Write Once Run Anywhere)
+		 * 		JVM will translate bytecode into machine specific code!
+		 * 		The JVM is very lightweight and can be run on most systems. 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+	}
+	
+
+}
